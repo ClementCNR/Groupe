e0ce36cee6c21d4ce8ceeb752ee3c59a48ddf90d
Mili <muhamed.mili92@gmail.com>
2025-05-30 23:15:10 +0200

    fix front

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -7,17 +7,16 @@ import { useAuth } from '../context/AuthContext';
 export default function Home() {
     const { user } = useAuth();
     const router = useRouter();
-
     useEffect(() => {
         if (!user) {
             router.push('/login');
         } else {
-            // Redirection selon le rôle si besoin
             if (user.role === 'EMPLOYEE') router.push('/dashboard/employee');
             else if (user.role === 'SECRETARY') router.push('/dashboard/secretary');
             else if (user.role === 'MANAGER') router.push('/dashboard/manager');
         }
-    }, [user]);
+    }, [user, router]); 
+
 
     return null; // Pas de contenu ici, on redirige automatiquement
 }
